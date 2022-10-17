da14c384a9617ef5a2f24fdf90e19886bca53223
Edgar S. Hurtado <esanchez@m-lean.com>
2022-10-17 08:54:35 +0200

    Create UseXibFiles project

---
+++ UseXibFiles/UseXibFiles/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UseXibFiles
+//
+//  Created by Edgar Sánchez Hurtado on 17/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var xibContainer: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
